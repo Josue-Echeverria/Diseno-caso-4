f2b0ec5dc5baf7cec33c9f8f1628504189f2fda2
Josue <jecheverria@estudiantec.cr>
2024-09-29 20:28:14 -0600

    Auto stash before merge of "main" and "origin/main"

--- app/app.js
+++ app/app.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
+const { createClient } = require('redis');
+const express = require("express");
+const app = express();
+
 const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://root:password@mongo:27017'; // Cambia esto si es necesario
 const dbName = 'db_Tvet';
 const collectionName = 'Pets';
 
+const redisClient = createClient({
+  url: 'redis://redis-master:6379' // Cambia esto a la URL de tu contenedor de Redis
+});
+redisClient.connect().then(async () => {
+  await redisClient.set('key', 'value');
+  const value = await redisClient.get('key');
+  console.log(value); // Debería imprimir 'value'
+});
+const PORT = process.env.PORT || 3000;
 // Leer el archivo JSON
 let jsonData;
 try {
@@ -36,5 +49,60 @@ try {
         await client.close();
     }
 })();
+app.get("/documents", async (req, res) => {
+  try{
+    await client.connect();
+    const database = client.db('TPet');
+    const collection = database.collection('HistorialMascotas');
+    const totalDocuments = await collection.countDocuments();
+    const sampleSize = Math.ceil(totalDocuments * 0.35);
+    const result = await collection.aggregate([
+      { $sample: { size: sampleSize } }
+  ]).toArray();
+    res.json(result);
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data.');
+  } finally {
+    await client.close();
+}
+});
 
+// Middleware para verificar el caché
+function checkCache(req, res, next) {
+  const { param1, param2 } = req.query;
+  const cacheKey = `${param1}:${param2}`;
+
+  redisClient.get(cacheKey, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  });
+}
+
+app.get("/documents/redis",  checkCache, async (req, res) => {
+  try{
+    await client.connect();
+    const database = client.db('TPet');
+    const collection = database.collection('HistorialMascotas');
+    const totalDocuments = await collection.countDocuments();
+    const sampleSize = Math.ceil(totalDocuments * 0.3);
+    const result = await collection.aggregate([
+      { $sample: { size: sampleSize } }
+  ]).toArray();
+    redisClient.setex(cacheKey, 60, JSON.stringify(result.slice(0,10)));
+    res.json(result);
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching data.');
+  } finally {
+    await client.close();
+}
+});
 
